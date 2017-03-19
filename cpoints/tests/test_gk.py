4d73328748f867ff8f5ae00590ba9b384c80d3c2
Thomas Spura <thomas.spura@gmail.com>
2017-03-19 02:03:29 +0100

    Add test to calculated cumulants of gk file

--- cpoints/tests/test_gk.py
+++ cpoints/tests/test_gk.py
@@ -2,6 +2,8 @@
 """
 Test grand canonical simulation data
 """
+import numpy as np
+from numpy.testing import assert_almost_equal
 import os
 import pytest
 
@@ -21,6 +23,21 @@ def test_read_gk():
     data = cp.Statistics(ensemble="grand_canonical")
     data.from_mc(get_test_file())
 
+    data2 = cp.read_mc(get_test_file())
+    print(data.data.head())
+    print(data2.data.head())
+    assert np.sum(data.data - data2.data).all() == 0.0
+
     with pytest.raises(AssertionError):
         data = cp.Statistics(ensemble="NPT")
         data.from_mc(get_test_file())
+
+def test_cumulants():
+    """ Test cumulants of gk file.
+    """
+    data = cp.Statistics(ensemble="grand_canonical")
+    data.from_mc(get_test_file())
+
+    print("K2/K4:", data.K2, data.K4)
+    assert_almost_equal(data.K2, 1.25963602195)
+    assert_almost_equal(data.K4, 1.66009790754)
